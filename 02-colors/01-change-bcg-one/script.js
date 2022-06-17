9bb7e04a0fd3cf934cb3783737e4decf50ae2bb8
Levy Crols <103933738+levyCro@users.noreply.github.com>
2022-06-17 13:29:53 +0200

    created difrent background color with one loop and eventlistnerand switch statment

--- 02-colors/01-change-bcg-one/script.js
+++ 02-colors/01-change-bcg-one/script.js
@@ -1,8 +1,31 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
+(function () {
     // your code here
+    const buttons = document.querySelectorAll("button");
 
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+
+            switch (button) {
+                case (button = document.querySelector("#red")):
+                    document.body.style.backgroundColor = "red";
+                    break;
+                case (button = document.querySelector("#green")):
+                    document.body.style.backgroundColor = "green";
+                    break;
+                case (button = document.querySelector("#yellow")):
+                    document.body.style.backgroundColor = "yellow";
+                    break;
+                case (button = document.querySelector("#blue")):
+                    document.body.style.backgroundColor = "blue";
+                    break;
+                default:
+                    console.log("error");
+            }
+
+        });
+
+    });
 })();
